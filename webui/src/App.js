780f3893c15484320c6305ce4adc69964a9552c7
Nikolay Martyanov <nikolay@zededa.com>
2023-09-12 14:09:04 +0200

    devmate/webui: Add the Maintenance Section.
    The section contain the "Set Online/Offline" buttons.
    
    Also disable the buttons when not all fields necessary for the action are
    provided.
    
    Signed-off-by: Nikolay Martyanov <nikolay@zededa.com>

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import {
   Button,
+  Box,
   Collapse,
+  Grid,
   Table,
   TableBody,
   TableCell,
@@ -9,18 +11,14 @@ import {
   TableHead,
   TableRow,
   Paper,
-  TextField, Box,
+  TextField,
 } from '@mui/material';
-
 
 import axios from 'axios';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  palette: {
-    mode: 'light', // or 'light'
-  },
 });
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8001';
@@ -32,6 +30,7 @@ const App = () => {
   const [showAddDevice, setShowAddDevice] = useState(false); // New state to handle the collapse
   const [showStatus, setShowStatus] = useState(false);
   const [deviceUsernames, setDeviceUsernames] = useState({});
+
 
 
 
@@ -61,7 +60,7 @@ const App = () => {
 
   useEffect(() => {
     fetchDevices();
-  }, []);
+  }, );
 
   const handleApiCall = async (url, method, payload, successCallback) => {
     try {
@@ -138,107 +137,159 @@ const App = () => {
     }).then(r => console.log('Device added successfully.'));
    };
 
+  const handleUsernameChange = (event, deviceName) => {
+    setDeviceUsernames({
+      ...deviceUsernames,
+      [deviceName]: event.target.value,
+    });
+  };
+
+  const handleRelease = (deviceName) => {
+    if (!deviceUsernames[deviceName]) {
+      const errorMessage = 'Username is required to release a device.';
+      customSetStatusMessage(errorMessage, true);
+      return;
+    }
+    handleUsernameChange({target: {value: ''}}, deviceName);
+    handleAction('release', deviceName);
+  }
+
+  const handleOnline = (deviceName) => {
+    handleUsernameChange({target: {value: ''}}, deviceName);
+    handleAction('online', deviceName);
+  }
+
   return (
-    <div>
-      <Box m={3}>
-      <h1>Device Management System</h1>
-      <h2>Available Devices</h2>
-      <TableContainer component={Paper}>
-        <Table style={{tableLayout: "fixed"}}>
-          <TableHead>
-            <TableRow style={{fontWeight: "bold"}}>
-              <TableCell>Device Name</TableCell>
-              <TableCell>Model</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Reserved By</TableCell>
-              <TableCell>Actions</TableCell>
-              <TableCell>Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {devices.map((device) => {
-
-              const handleTextFieldChange = (event, deviceName) => {
-                setDeviceUsernames({
-                  ...deviceUsernames,
-                  [deviceName]: event.target.value,
-                });
-              };
-
-              return (
-              <TableRow key={device.name}>
-                <TableCell>{device.name}</TableCell>
-                <TableCell>{device.model}</TableCell>
-                <TableCell>{device.status}</TableCell>
-                <TableCell>
-                    {device.status === 'free' ? (
-                        <TextField label="Username" onChange={(event) => handleTextFieldChange(event, device.name)}/>
-                    ) : (
-                        device.user
-                    )}
-                </TableCell>
-                <TableCell>
-                  {device.status === 'free' ? (
-                      <Button
-                          variant="contained"
-                          color="success"
-                          onClick={() => handleReserve(device.name)}
-                          disabled={!deviceUsernames[device.name]} >
-                        Reserve
-                      </Button>
-                  ) : (
-                      device.status === 'reserved' && (
-                          <Button variant="contained" onClick={() => handleAction('release', device.name)}>
-                            Release
-                          </Button>
-                      )
-                  )}
-              </TableCell>
-              <TableCell>
-                <Button variant="contained" color="secondary" onClick={() => handleDelete(device.name)}>
-                  Delete
-                </Button>
-              </TableCell>
-              </TableRow>
-            );})}
-          </TableBody>
-        </Table>
-      </TableContainer>
       <div>
         <Box m={3}>
-        <Button variant="outlined" color="primary" onClick={() => setShowAddDevice(!showAddDevice)}>
-          {showAddDevice ? 'Hide Add Device' : 'Show Add Device'}
-        </Button>
-        <Collapse in={showAddDevice}>
-          <div style={{ marginTop: 20 }}>
-            <h2>Add Device</h2>
-            <TextField
-                label="Device Name"
-                value={newDevice.device}
-                onChange={(e) => setNewDevice({ ...newDevice, device: e.target.value })}
-                style={{ marginRight: 10 }}
-            />
-            <TextField
-                label="Model"
-                value={newDevice.model}
-                onChange={(e) => setNewDevice({ ...newDevice, model: e.target.value })}
-                style={{ marginRight: 10 }}
-            />
-            <Button variant="contained" color="primary" onClick={handleAddDevice}>
-              Add Device
-            </Button>
+          <h1>Device Management System</h1>
+          <h2>Available Devices</h2>
+          <TableContainer component={Paper}>
+            <Table style={{tableLayout: "fixed"}}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Device Name</TableCell>
+                  <TableCell>Model</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Reservation</TableCell>
+                  <TableCell>Maintenance</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {devices.map((device) => {
+
+
+                  return (
+                      <TableRow key={device.name}>
+                        <TableCell>{device.name}</TableCell>
+                        <TableCell>{device.model}</TableCell>
+                        <TableCell>{device.status}</TableCell>
+                        <TableCell>
+                          <Grid container alignItems="center">
+                            <Grid item xs={6}>
+
+                              {device.status === 'free' ? (
+                                  <TextField label="Username"
+                                             onChange={(event) => handleUsernameChange(event, device.name)}/>
+                              ) : (
+                                  device.user
+                              )}
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Box display="flex" justifyContent="flex-end">
+                                {device.status === 'free' ? (
+                                    <Button
+                                        variant="contained"
+                                        color="success"
+                                        onClick={() => handleReserve(device.name)}
+                                        disabled={!deviceUsernames[device.name]}>
+                                      Reserve
+                                    </Button>
+                                ) : (
+                                    device.status === 'reserved' && (
+                                        <Button variant="contained"
+                                                onClick={() => handleRelease(device.name)}>
+                                          Release
+                                        </Button>
+                                    )
+                                )}
+                              </Box>
+                            </Grid>
+                          </Grid>
+                        </TableCell>
+                        <TableCell>
+                          {(
+                              <Box display={"flex"} alignItems={"center"} sx={{gap: 1}}>
+                                {device.status !== "offline" ? (
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => handleAction("offline", device.name)}
+                                    >
+                                      Set Offline
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => handleOnline(device.name)}
+                                    >
+                                      Set Online
+                                    </Button>
+                                )}
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={() => handleDelete(device.name)}
+                                >
+                                  Delete
+                                </Button>
+                              </Box>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div>
+            <Box mt={3}>
+              <Button variant="outlined" color="primary" onClick={() => setShowAddDevice(!showAddDevice)}>
+                {showAddDevice ? 'Hide Add Device' : 'Show Add Device'}
+              </Button>
+              <Collapse in={showAddDevice}>
+                <div style={{marginTop: 20}}>
+                  <h2>Add Device</h2>
+                  <Box alignItems={"center"} display={"flex"} sx={{gap: 1}}>
+                  <TextField
+                      label="Device Name"
+                      value={newDevice.device}
+                      onChange={(e) => setNewDevice({...newDevice, device: e.target.value})}
+                      style={{marginRight: 10}}
+                  />
+                  <TextField
+                      label="Model"
+                      value={newDevice.model}
+                      onChange={(e) => setNewDevice({...newDevice, model: e.target.value})}
+                      style={{marginRight: 10}}
+                  />
+                  <Button variant="contained" color="primary" onClick={handleAddDevice}>
+                    Add Device
+                  </Button>
+                  </Box>
+                </div>
+              </Collapse>
+            </Box>
           </div>
-        </Collapse>
-          </Box>
+          {showStatus && (
+              <div>
+                <h2>Status Messages</h2>
+                <p>{statusMessage}</p>
+              </div>
+          )}
+        </Box>
       </div>
-      {showStatus && (
-            <div>
-              <h2>Status Messages</h2>
-              <p>{statusMessage}</p>
-            </div>
-      )}
-    </Box>
-    </div>
   );
 };
 
