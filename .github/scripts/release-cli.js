28196667861c886a2168d31e1847d8ec5c36d34c
Nikolay Martyanov <nikolay@zededa.com>
2023-09-15 00:44:30 +0200

    devmate: Simplify CLI Distribution through Automated GitHub Releases.
    This commit optimizes the CI/CD pipeline to facilitate the distribution of CLI
    binaries through GitHub releases. This approach ensures that all binaries can
    be downloaded from a predictable URL generated by the automated release
    process.
    
    Add a new script (.github/scripts/release-cli.js) to automate GitHub release
    creation and asset upload. The script will generate releases tagged in the
    YY.MM.dd.HASH format and will include a single tar.gz asset
    ('devmate-cli.tar.gz') containing all CLI artifacts.
    
    Remove the redundant workflow for fetching artifact URLs (get-cli-urls.yml), as
    its functionality is now covered by the new release automation script.
    
    Update the build workflow (publish-cli.yml) to incorporate these new release
    and archiving steps.
    
    Signed-off-by: Nikolay Martyanov <nikolay@zededa.com>

---
+++ .github/scripts/release-cli.js
@@ -0,0 +1,54 @@
+const os = require("os");
+module.exports = async function({ github, context, core }) {
+    const fs = require('fs');
+    const path = require('path');
+
+    // Construct a tag name in a format of YY.MM.dd.HASH
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear().toString().substr(-2);
+    const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const currentDay = currentDate.getDate().toString().padStart(2, '0');
+    const commitHash = context.sha.substring(0, 4);  // Taking first 4 characters of commit hash
+    const tagName = `${currentYear}.${currentMonth}.${currentDay}.${commitHash}`;
+
+    let release;
+    try {
+        release = await github.rest.repos.createRelease({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            tag_name: tagName,
+            name: `DevMate CLI - ${tagName}`,
+        });
+    } catch (e) {
+        console.error("Failed to create release: ", e);
+        // Force fail the action
+        core.setFailed(e.message);
+        return;
+    }
+
+    // Add the devmate-cli.tar.gz file to the release
+    const fileName = 'devmate-cli.tar.gz';
+    const filePath = path.join('.', fileName);
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+        console.error(`File ${fileName} does not exist`);
+        core.setFailed(`File ${fileName} does not exist`);
+        return;
+    }
+
+    // read content of the file
+    const fileData = fs.readFileSync(filePath);
+
+    try {
+        await github.rest.repos.uploadReleaseAsset({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            release_id: release.data.id,
+            name: fileName,
+            data: fileData
+        });
+    } catch (e) {
+        console.error(`Failed to upload asset ${fileName}: `, e);
+        core.setFailed(e.message);
+    }
+};
